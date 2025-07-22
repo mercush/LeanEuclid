a7047ac09a5c72cdea680b1f1f786a37193b8481
mercush <barba@mit.edu>
2025-07-22 23:11:21 +0000

    running on vm

--- AutoFormalization/lean.py
+++ AutoFormalization/lean.py
@@ -137,6 +137,7 @@ class GenLMModel:
     def __init__(self, model_name: str, temperature: float = 1., max_tokens: int = 300, n_particles: int = 10):
         self.llm = PromptedLLM.from_name(model_name, temperature=temperature, 
             engine_opts={
+                "tensor_parallel_size": 8,
                 "max_model_len": 2*4096,
                 })
         self.lean_potential = LeanPotential(self.llm)
